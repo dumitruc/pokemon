6d86773e3170bbbf5bc558de62c65f11a973e3cf
dumitruc <diakon@gmail.com>
2021-12-19 19:49:50 +0000

    Added some more production considerations

--- README.md
+++ README.md
@@ -11,7 +11,23 @@ This will return basic information of a pokemon
 ## /pokemon/translated/{pokemon-name}
 This will return the pokemon with translated description
 
+# Development setup
+ * Install [git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
+ * Install [Java JDK](https://docs.oracle.com/en/java/javase/17/install/overview-jdk-installation.html)
+ * Install [maven](https://maven.apache.org/install.html)
+ * Install [docker](https://docs.docker.com/get-docker/)
+
 # Getting Started
+
+## Download the repository locally
+```shell
+git clone https://github.com/dumitruc/pokemon.git
+```
+## Go inside the repository
+```shell
+cd pokemon
+```
+This is your working directory home, all the commands should be executed form this folder.
 
 ## Build the service as a jar
 ```shell
@@ -28,4 +44,5 @@ docker run --rm -p8080:8080 pokemon
 
 ## Call the service:
 http://localhost:8080/pokemon/mewtwo
+
 http://localhost:8080/pokemon/translated/mewtwo
--- src/main/java/com/dumitruc/training/pokemon/controllers/PokemonController.java
+++ src/main/java/com/dumitruc/training/pokemon/controllers/PokemonController.java
@@ -5,6 +5,9 @@ import com.dumitruc.training.pokemon.exceptions.UnableToTranslate;
 import com.dumitruc.training.pokemon.model.PokemonSummary;
 import com.dumitruc.training.pokemon.services.PokemonInfoService;
 import com.dumitruc.training.pokemon.services.PokemonTranslatorService;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,23 +20,31 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.constraints.NotNull;
 
 @RestController
+@Timed
 public class PokemonController {
 
     private final PokemonTranslatorService pokemonTranslatorService;
     private final PokemonInfoService pokemonInfoService;
 
+    private Counter infoVisitCounter;
+    private Counter translateVisitCounter;
+
     public static final Logger logger = LoggerFactory.getLogger(PokemonController.class);
 
     @Autowired
     public PokemonController(PokemonTranslatorService pokemonTranslatorService,
-                             PokemonInfoService pokemonInfoService) {
+                             PokemonInfoService pokemonInfoService, MeterRegistry registry) {
         this.pokemonTranslatorService = pokemonTranslatorService;
         this.pokemonInfoService = pokemonInfoService;
+        this.infoVisitCounter = registry.counter("info_visit_counter");
+        this.translateVisitCounter = registry.counter("translate_visit_counter");
     }
 
     @ResponseStatus(value = HttpStatus.OK)
     @RequestMapping("/pokemon/{name}")
+    @Timed
     public PokemonSummary pokemonInfo(@NotNull @PathVariable String name) {
+        infoVisitCounter.increment();
         PokemonSummary pokemonSummary = null;
         logger.info("Retrieve base info for: {}", name);
         try {
@@ -48,7 +59,9 @@ public class PokemonController {
 
     @ResponseStatus(value = HttpStatus.OK)
     @RequestMapping("/pokemon/translated/{name}")
+    @Timed
     public PokemonSummary pokemonTranslated(@NotNull @PathVariable String name) {
+        translateVisitCounter.increment();
         logger.info("Retrieve translation for: {}", name);
         PokemonSummary pokemonSummary = pokemonInfo(name);
         try {
